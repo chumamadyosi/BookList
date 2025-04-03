b3b9d2239e6ccf9bcf20a4edf33d67657276dbd8
Chuma Madyosi <madyosi21@gmail.com>
2025-04-03 20:12:26 +0200

    feat: add book and athours - seed data

--- Backend/BookList/BookList.Data/BookListDbContext.cs
+++ Backend/BookList/BookList.Data/BookListDbContext.cs
@@ -22,6 +22,7 @@ namespace BookList.Data
             modelBuilder.ApplyConfiguration(new UserConfiguration());
 
             CreateSeedUsers(modelBuilder);
+            CreateSeedAuthorsWithBooks(modelBuilder);
         }
 
         public DbSet<Book> Books { get; set; }
@@ -45,6 +46,44 @@ namespace BookList.Data
                    PasswordHash = "$2a$11$TaBHmICwPnLQUX/Qb/Msc.Di9okVQ9xdC15F7aGVah9M3ehIhZE4e", // Hash of "User@123"
                    Role = "User"
                });
+        }
+        private static void CreateSeedAuthorsWithBooks(ModelBuilder modelBuilder)
+        {
+            // Seed Authors
+            modelBuilder.Entity<Author>().HasData(
+                new Author
+                {
+                    AuthorId = 1,
+                    Name = "J.K. Rowling"
+                },
+                new Author
+                {
+                    AuthorId = 2,
+                    Name = "George R. R. Martin"
+                }
+            );
+
+            // Seed Books
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    BookId = 1,
+                    Title = "Harry Potter and the Philosopher's Stone",
+                    AuthorId = 1, // Link to J.K. Rowling
+                    PublishedYear = 1997,
+                    ISBN = "9780747532743",
+                    Description = "The first book in the Harry Potter series."
+                },
+                new Book
+                {
+                    BookId = 2,
+                    Title = "A Game of Thrones",
+                    AuthorId = 2, // Link to George R. R. Martin
+                    PublishedYear = 1996,
+                    ISBN = "9780553103540",
+                    Description = "The first book in the A Song of Ice and Fire series."
+                }
+            );
         }
     }
 }
---
+++ Backend/BookList/BookList.Data/Migrations/20250403175635_SeedBooksAndAuthors.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookList.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBooksAndAuthors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "AuthorId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "J.K. Rowling" },
+                    { 2, "George R. R. Martin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "AuthorId", "Description", "ISBN", "PublishedYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "The first book in the Harry Potter series.", "9780747532743", 1997, "Harry Potter and the Philosopher's Stone" },
+                    { 2, 2, "The first book in the A Song of Ice and Fire series.", "9780553103540", 1996, "A Game of Thrones" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "AuthorId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "AuthorId",
+                keyValue: 2);
+        }
+    }
+}
