e14ed9f046cfd5db84a94b6653059db7133ae3f3
Chuma Madyosi <madyosi21@gmail.com>
2025-04-03 18:17:39 +0200

    feat: add user repository

---
+++ Backend/BookList/BookList.Data/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookList.Data.Entities;
+using BookList.Data.Repository.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList.Data.Repository
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetByUsernameAsync(string username);
+    }
+}
---
+++ Backend/BookList/BookList.Data/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookList.Data.Entities;
+using BookList.Data.Repository.Generic;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList.Data.Repository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly BookListDbContext _context;
+        public UserRepository(BookListDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+    }
+}
